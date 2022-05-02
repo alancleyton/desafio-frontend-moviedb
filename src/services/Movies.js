d6f64f20b5f6bf9826e5876e77bcc737a0a30abd
Alan Cleyton <alancleyton67@gmail.com>
2022-05-02 11:21:59 -0300

    feat: add getMovieDetails to movies services

--- src/services/Movies.js
+++ src/services/Movies.js
@@ -6,4 +6,10 @@ export class Movies {
       `/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR&page=1`,
     );
   }
+
+  static getMovieDetails(movieId) {
+    return AXIOS.get(
+      `/movie/${movieId}?api_key=${process.env.REACT_APP_API_KEY}&language=pt-BR`,
+    );
+  }
 }
