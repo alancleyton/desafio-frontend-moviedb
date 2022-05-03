6eec8e30bd6bdd74c03fc28a4afd59a366e5b75a
Alan Cleyton <alancleyton67@gmail.com>
2022-05-03 16:09:31 -0300

    feat: list movies by search

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -1,16 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { MdMovie } from 'react-icons/md';
 import { FiSearch } from 'react-icons/fi';
-import { Container, Field } from './SearchField.styles';
+import debounce from 'lodash.debounce';
+import {
+  Container,
+  Field,
+  FieldDropDown,
+  MoviesResult,
+} from './SearchField.styles';
+import { Movies } from '../../services/Movies';
 
-const SearchField = () => (
-  <Container>
-    <Field>
-      <input type="text" placeholder="Pesquisar por filmes" />
-      <button type="button">
-        <FiSearch size={18} color="#ffffff" />
-      </button>
-    </Field>
-  </Container>
-);
+const formattedReleaseDate = date => date.substring(0, 4);
+
+const SearchField = () => {
+  const [searchQuery, setSearchQuery] = useState(null);
+  const [searchResult, setSearchResult] = useState([]);
+  const [inputFocused, setInputFocused] = useState(false);
+  const updateSearchQuery = event => setSearchQuery(event.target.value);
+  const debouncedOnChange = debounce(updateSearchQuery, 500);
+
+  const onInputFocus = () => setInputFocused(true);
+  const onInputBlur = () => setInputFocused(false);
+
+  useEffect(() => {
+    async function handleSearch() {
+      if (searchQuery) {
+        const response = await Movies.searchMovies(searchQuery);
+        setSearchResult(response.data.results);
+      }
+    }
+
+    handleSearch();
+  }, [searchQuery]);
+
+  const moviePoster = movie =>
+    movie.poster_path ? (
+      <img
+        src={`https://www.themoviedb.org/t/p/original/${movie.poster_path}`}
+        alt={movie.title}
+      />
+    ) : (
+      <MdMovie size={28} color="#a3a3a3" />
+    );
+
+  const movieReleaseDate = movie =>
+    movie.release_date ? formattedReleaseDate(movie.release_date) : ' ';
+
+  return (
+    <Container>
+      <Field>
+        <input
+          type="text"
+          placeholder="Pesquisar por filmes"
+          onChange={debouncedOnChange}
+          onFocus={onInputFocus}
+          onBlur={onInputBlur}
+        />
+        <button type="button">
+          <FiSearch size={18} color="#ffffff" />
+        </button>
+      </Field>
+
+      <FieldDropDown isVisible={inputFocused && searchQuery}>
+        <MoviesResult>
+          {searchResult.length > 0 &&
+            searchResult.map(movie => (
+              <li key={movie.id}>
+                <figure>{moviePoster(movie)}</figure>
+                <div>
+                  <h6>{movie.title}</h6>
+                  <span>{movieReleaseDate(movie)}</span>
+                </div>
+              </li>
+            ))}
+        </MoviesResult>
+      </FieldDropDown>
+    </Container>
+  );
+};
 
 export default SearchField;
--- src/components/SearchField/SearchField.styles.js
+++ src/components/SearchField/SearchField.styles.js
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
+import { lighten } from 'polished';
 import {
+  primaryGray,
   secondaryGray,
   secondarySilverGray,
   secondaryYellow,
+  white,
 } from '../../styles/colors';
 
 export const Container = styled.div`
@@ -54,5 +57,90 @@ export const Field = styled.div`
   button:hover,
   button:focus {
     filter: brightness(150%);
+  }
+`;
+
+export const FieldDropDown = styled.div`
+  position: absolute;
+  width: 100%;
+  height: auto;
+  max-height: 600px;
+  min-height: 200px;
+  top: 50px;
+  background-color: ${primaryGray};
+  z-index: 1000;
+  display: ${props => (props.isVisible ? 'block' : 'none')};
+  border-radius: 4px;
+  overflow-y: auto;
+  scrollbar-width: auto;
+  &::-webkit-scrollbar {
+    width: 10px;
+  }
+  &::-webkit-scrollbar-track {
+    background: transparent;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${secondaryGray};
+    border-radius: 16px;
+    border: 4px solid transparent;
+    min-height: 86px;
+  }
+`;
+
+export const MoviesResult = styled.ul`
+  margin: 0;
+  padding: 0;
+  li {
+    padding: 0.75rem;
+    display: flex;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    &:not(:last-child) {
+      border-bottom: 1px solid ${secondaryGray};
+    }
+    &:hover {
+      background-color: ${lighten(0.2, primaryGray)};
+    }
+    figure {
+      margin: 0 8px 0 0;
+      height: 80px;
+      width: 60px;
+      border-radius: 4px;
+      background-color: ${secondaryGray};
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        border-radius: 4px;
+      }
+    }
+    div {
+      h6 {
+        margin-bottom: 0.5rem;
+        font-size: 14px;
+        font-weight: 600;
+        color: ${white};
+      }
+      span {
+        display: block;
+        font-size: 12px;
+        font-weight: 500;
+        color: ${white};
+      }
+    }
+  }
+`;
+
+export const MovieNotFound = styled.div`
+  text-align: center;
+  p {
+    margin-top 4rem;
+    display: block;
+    font-size: 14px;
+    font-weight: 500;
+    color: #ff2400;
   }
 `;
